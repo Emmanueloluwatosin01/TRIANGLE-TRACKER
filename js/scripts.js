d79c5c3e53aea1dbb72a84b8ba0870ba183616f6
Emmanuel <oluwatosinmanny01@gmail.com>
2022-05-09 14:39:04 -0700

    The body Structure is completed

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// $(document).ready(function() {
+//  $("#school").submit(function(event){
+//   event.preventdefault();
+//   const Side1 = parseInt($("#Side-1").val());
+//   const Side2 = parseInt($("Side-2").val());
+//   const Side3 = parseInt($("Side-3").val());
+
+//   if(Side1 === Side2 && Side2 === Side3 && Side1 === Side3){
+        
+//   }
+// })
+// });
