ba9dd70a04f5bb3adb38920f08d4a09db7d0da6c
Emmanuel <oluwatosinmanny01@gmail.com>
2022-05-10 03:33:23 -0700

    js scripts val has been created.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,34 @@
-// $(document).ready(function() {
-//  $("#school").submit(function(event){
-//   event.preventdefault();
-//   const Side1 = parseInt($("#Side-1").val());
-//   const Side2 = parseInt($("Side-2").val());
-//   const Side3 = parseInt($("Side-3").val());
+$(document).ready(function() {
+ $("#school").submit(function(event){
+  event.preventDefault();
+  const Side1 = parseInt($("#Side-1").val());
+  const Side2 = parseInt($("#Side-2").val());
+  const Side3 = parseInt($("#Side-3").val());
 
-//   if(Side1 === Side2 && Side2 === Side3 && Side1 === Side3){
-        
+//   if(Side1 === Side2 && Side2=== Side3 && Side1=== Side3){
+//      $("#equilat").show();
+//      $("#isoc").hide();
+//      $("#scal").hide();
+//      $("#ntn").hide();
 //   }
+//   else if (((Side1 + Side2) <= Side3) || ((Side2 + Side3) <= Side1) || ((Side1 +Side3) <= Side2)){
+//     $("#ntn").show();
+//     $("#scal").hide();
+//     $("#isoc").hide();
+//     $("#equilat").hide();
+//   }
+//   else if(Side1 === Side2 || Side2 === Side3 || Side1 === Side3){
+//     $("#isoc").show();
+//     $("#equilat").hide();
+//     $("#scal").hide();
+//      $("#ntn").hide();
+//   }
+//   else if(Side1 !== Side2 && Side2 !== Side3 && Side1 !== Side3){
+//     $("#scal").show();
+//     $("#isoc").hide();
+//     $("#equilat").hide();
+//     $("#ntn").hide();
+//   }
+  
 // })
 // });
